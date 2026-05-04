844ecf48d0fdad67a1b91ae110b221c93088ca3c
nichelledsouza2008-beep <nichelledsouza2008@gmail.com>
2026-05-04 11:09:35 +0530

    add GroqService for AI outfit suggestions

--- src/main/java/com/wearther/App.java
+++ src/main/java/com/wearther/App.java
@@ -1,18 +1,33 @@
 package com.wearther;
 import io.github.cdimascio.dotenv.Dotenv;
+import java.util.Scanner;
 public class App {
-    public static void main (String args[]) {
+    public static void main (String args[]) 
+    {
         Dotenv dotenv = Dotenv.load();
         String weatherkey = dotenv.get("OPENWEATHER_API_KEY");
         String groqkey = dotenv.get("GROQ");
-        
-        WeatherService ws = new WeatherService(weatherkey);
 
-        try{
-            String weatherInfo = ws.getWeather("Coimbatore");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter the city you reside in");
+        String city = in.nextLine();
+
+        WeatherService ws = new WeatherService(weatherkey);
+        GroqService gs = new GroqService(groqkey);
+
+        try
+        {
+            String weatherInfo = ws.getWeather(city);
+            System.out.println("Weather Information:");
             System.out.println(weatherInfo);
-        } catch (Exception e) {
+            String outfitsuggestion = gs.getSuggestion(weatherInfo);
+            System.out.println("According to the weather, here is a suggestion for your outfit:");
+            System.out.println(outfitsuggestion);
+        } 
+        catch (Exception e) 
+        {
             System.out.println("Error fetching weather data: " + e.getMessage());
         }
+        in.close(); //ask why
     }
 }
